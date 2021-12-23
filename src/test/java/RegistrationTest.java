3b924c76d3e63cb2b0a8cba07eb5413a426578f7
jovana86 <basaric.jovana@yahoo.com>
2021-12-23 14:28:59 +0100

    poslednje izmene

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -3,14 +3,12 @@ import org.testng.annotations.Test;
 public class RegistrationTest extends BaseTest{
 
     @Test
-    public void RegistracijaSaValidnimPodacima(){
+    public void RegistracijaSaValidnimPodacimaTest(){
         driver=openChromeDriver ();
 
         try {
 
             RegistrationPage registrationPage = new RegistrationPage ( driver );
-
-
 
             registrationPage.clickRegistracijaButton ();
             registrationPage.registracijaUnesiIme ();
@@ -34,6 +32,99 @@ public class RegistrationTest extends BaseTest{
 
 
 //todo uradi asert za negativan scenario
+
+
+
+        }finally {
+            driver.quit ();
+        }
+    }
+
+    @Test
+    public void registracijaSaNevalidnomEmailAdresomTest(){
+        driver = openChromeDriver ();
+        try {
+            RegistrationPage registrationPage = new RegistrationPage ( driver );
+
+            registrationPage.clickRegistracijaButton ();
+            registrationPage.registracijaUnesiIme ();
+            registrationPage.registracijaUnesiPrezime ();
+            registrationPage.registracijaUnesiNevalidniEmail ();
+            registrationPage.registracijaUnesiTelefon ();
+            registrationPage.registracijaUnesiGrad ();
+            registrationPage.registracijaUnesiPostanskiBroj ();
+            registrationPage.registracijaUnesiAdresu ();
+            registrationPage.registracijaUnesiBrojUlice ();
+            registrationPage.registracijaUnesiLozinku ();
+            registrationPage.registracijaPonovoUnesiLozinku ();
+            registrationPage.registracijaOznaciPolZenski ();
+            registrationPage.registracijaOznaciSlazemSeSaUslovimaKoriscenja ();
+            registrationPage.registracijaPotvrdiRegistracijuButtonClick ();
+            registrationPage.registracijaDaLiJeErrorMessagePrisutan ();
+            registrationPage.registracijaDaLiJeEmailErrorMessagePrisutan ();
+
+
+        }finally {
+            driver.quit ();
+        }
+
+    }
+
+    @Test
+    public void registracijaSaNevalidnimBrojemTelefonaILozinkomSaManjeOd6KarakteraTest(){
+        driver = openChromeDriver ();
+
+        try {
+            RegistrationPage registrationPage = new RegistrationPage ( driver );
+
+            registrationPage.clickRegistracijaButton ();
+            registrationPage.registracijaUnesiIme ();
+            registrationPage.registracijaUnesiPrezime ();
+            registrationPage.registracijaUnesiEmail ();
+            registrationPage.registracijaUnesiNevalidniBrojTelefona ();
+            registrationPage.registracijaUnesiGrad ();
+            registrationPage.registracijaUnesiPostanskiBroj ();
+            registrationPage.registracijaUnesiAdresu ();
+            registrationPage.registracijaUnesiBrojUlice ();
+            registrationPage.registracijaUnesiDo6KarakteraLozinku ();
+            registrationPage.registracijaPonovoUnesiDo6KarakteraLozinku ();
+            registrationPage.registracijaOznaciPolZenski ();
+            registrationPage.registracijaOznaciSlazemSeSaUslovimaKoriscenja ();
+            registrationPage.registracijaOznaciLoyaltyRequest ();
+            registrationPage.registracijaPotvrdiRegistracijuButtonClick ();
+            registrationPage.registracijaDaLiJeErrorMessagePrisutan ();
+            registrationPage.registracijaDaLiJeTelefonErrorMessagePrisutan ();
+            registrationPage.registracijaDaLiJeLozinkaDo6KarakteraErrorMessagePrisutan ();
+
+        }finally {
+            driver.quit ();
+        }
+    }
+
+    @Test
+    public void registracijaSaPogresnimPostanskimBrojemINjegoveErrorPoruke(){
+        driver = openChromeDriver ();
+        try {
+            RegistrationPage registrationPage = new RegistrationPage ( driver );
+
+            registrationPage.clickRegistracijaButton ();
+            registrationPage.registracijaUnesiIme ();
+            registrationPage.registracijaUnesiPrezime ();
+            registrationPage.registracijaUnesiEmail ();
+            registrationPage.registracijaUnesiTelefon ();
+            registrationPage.registracijaUnesiGrad ();
+            registrationPage.registracijaNevalidanPostanskiBrojIErrorPoruke ();
+//            registrationPage.registracijaPostanskiBrojSa5NevalidnihKaraktera ();
+            registrationPage.registracijaUnesiAdresu ();
+//            registrationPage.registracijaDaLiJePostanskiBrojSa5NevalidnihKarakteraErrorMessagePrisutan ();
+            registrationPage.registracijaUnesiBrojUlice ();
+            registrationPage.registracijaUnesiLozinku ();
+            registrationPage.registracijaPonovoUnesiLozinku ();
+            registrationPage.registracijaOznaciPolZenski ();
+            registrationPage.registracijaOznaciSlazemSeSaUslovimaKoriscenja ();
+            registrationPage.registracijaOznaciLoyaltyRequest ();
+            registrationPage.registracijaPotvrdiRegistracijuButtonClick ();
+            registrationPage.registracijaDaLiJeErrorMessagePrisutan ();
 
 
 
